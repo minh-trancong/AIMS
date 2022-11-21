5dc7a32583f1e19a8b7af0e221c9aec3944718d6
Tran Cong Minh <tc.tranminh@gmail.com>
2022-11-21 09:29:55 +0700

    Initial commit

---
+++ src/Package1/Aims.java
@@ -0,0 +1,10 @@
+package Package1;
+
+public class Aims {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author minh
+ *
+ */
+module AimsProject {
+}
